51cfaef89e281410826c8e5f69a31a84b416045e
Erzsebeteemk <88439357+Erzsebeteemk@users.noreply.github.com>
2021-09-15 15:50:39 +0200

    ikon színez még nincs kész

--- script.js
+++ script.js
@@ -16,21 +16,26 @@ $("#fogomb").click (()  => {
 
             $("ul").append(` <li   class="sor"  id="sor${hanyadiksor}"   data-szamozo="${hanyadiksor}">
                                 ${szoveg}
-                                <button class="ikon">  <i class="fas fa-space-shuttle"></i>   </button>
+                                <button class="ikon"  data-szamozo="${hanyadiksor}" >  <i class="fas fa-space-shuttle"></i>   </button>
                                 <button class="torl" id="torl${hanyadiksor}"  data-szamozo="${hanyadiksor}" >   Törlés      </button>
-                                <button class="jegy" id="jegy${hanyadiksor}"  data-szamozo="${hanyadiksor}" >   J     </button>
-                                <button class="megn" id="megn${hanyadiksor}"  data-szamozo="${hanyadiksor}" >   M  </button>
-                                <button class="rend" id="rend${hanyadiksor}"  data-szamozo="${hanyadiksor}" >   S     </button>
-                                <button class="szin" id="szin${hanyadiksor}"  data-szamozo="${hanyadiksor}" >   Sz        </button>
                              </li> 
             `)
 }
 )
+                                /*
+                                <button class="jegy" id="jegy${hanyadiksor}"  data-szamozo="${hanyadiksor}" >   J     </button>
+                                <button class="megn" id="megn${hanyadiksor}"  data-szamozo="${hanyadiksor}" >   M  </button>
+                                <button class="rend" id="rend${hanyadiksor}"  data-szamozo="${hanyadiksor}" >   S     </button>
+                                <button class="szin" id="szin${hanyadiksor}"  data-szamozo="${hanyadiksor}" >   Sz        </button>
+                                */
 
 
 
+/*
+T: minta
+T: Dupla katt: piros-fekete betűszín-váltás.
+Ne töröld ki, mert ez minta.
 
-// T: Dupla katt: piros-fekete betűszín-váltás.
 let most = null
 $("ul").on("dblclick","li",(event) => {
                                         // console.log(event)
@@ -43,7 +48,7 @@ else if (`${most}` !== "rgb(0, 0, 0)")   {
     $(event.target).css("color", "rgb(0, 0, 0)")  }
 }
 )
-
+*/
 
 
 
@@ -72,6 +77,57 @@ $("ul").on("click","li",(event) => {
 }
 )
 
+
+// T: Színminta az inputmező felett (ikonhoz, betűszínhez kapcsolódik.
+let szinek = ["red", "blue", "green", "white", "yellow", "green", "black"] 
+
+
+szinek.forEach(function (szinikon) {
+    $(".kontenerikon").append("<div class='szinboxikon'> </div>")
+    $(".kontenerikon div:last-of-type").css("background-color", szinikon)
+}
+)
+
+
+
+
+
+// T: Ikonra kattintva a listaelem betűszíne változik meg.
+kattikon = 0
+let mitszinezikon = null
+
+$("ul").on("click",".ikon",(event) => {
+                                        console.log(event)
+    kattikon = kattikon + 1
+                                       console.log(kattikon)
+
+    mitszinezikon = $(event.currentTarget).attr("data-szamozo")
+                                        console.log(mitszinezikon)
+
+            if (kattikon === 1) {
+                $("#sor"+mitszinezikon).css("color", "red")  }       
+//                $(li).css("color", "red")    }
+            else if (kattikon === 2) {
+                $("#sor"+mitszinezikon).css("color", "blue")  }       
+//                $(event.target).css("background-color", "rgb(255, 204, 0)")     }
+            else if (katt === 3) {
+                $("#sor"+mitszinezikon).css("color", "green")  }       
+//                $(event.target).css("background-color", "rgb(255, 119, 0)")     }
+            else if (katt === 4) {
+                $("#sor"+mitszinezikon).css("color", "white")  }       
+//                $(event.target).css("background-color", "rgb(212, 230, 135)")   }
+            else if (katt === 5) {
+                $("#sor"+mitszinezikon).css("color", "yellow")  }       
+//               $(event.target).css("background-color", "#c5dae4")               }
+            else if (katt === 6) {
+                $("#sor"+mitszinezikon).css("color", "pink")  }       
+ //               $(event.target).css("background-color", "#fbebfe")              }
+            else if (katt === 7) {
+                $("#sor"+mitszinezikon).css("color", "black")         
+//            $(event.target).css("background-color", "rgb(255, 255, 255)")   
+    kattikon = 0                                                                    }
+}
+)
 
 
 

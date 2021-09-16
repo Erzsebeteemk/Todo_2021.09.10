f55b2b96c3e3f00054483fd722f12412914d1243
Erzsebeteemk <88439357+Erzsebeteemk@users.noreply.github.com>
2021-09-17 00:40:58 +0200

    Befejezett. Kis telefonra méretezve.
    Véletlenszerű háttérszín, hét szín sáv tömb-konténerben, 7 szín választás, input mező ürítés, törlés gombra az egész sort törli, ikon,

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Todo_2021.09.10
-
+Véletlenszerű háttérszín egy gombra kattintva (fg, random, egészrész)
+Input mező kiürítés egy gombra kattintással
+Telefonra méretezve már a felső része, és átvált nagyobb képernyőnél (320px width-ről 500px width-re)
+Színcsík: tömbből beolvassa a színeket (append div majd ezt beszínezi)
+Újabb li-t tesz a listába gombra kattintva
+Törlés gombra kattintva a kattintott sort mindenestül törli
+Dupla kattra két betűszín között vált li-n: ez mintaként szerepel
+Szimpla kattra 7 háttérszínt vált li-n
+Ikon
+Háttérszín véletlenszerűen változik
+2-féle fg-felírás
--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 
-
-
-
-
-// T: Főgombra kattintva mindig újabb li-t tesz az ul-be, szövege az, amit a felh. beír az input mezőbe.
+// T: Főgombra kattintva mindig újabb li-t tesz az ul-be, az input mezőből (input, val, li, ul)
 let hanyadiksor = 1
-let osszessor = 1
 let szoveg = null
 
 $("#fogomb").click (()  => {
 
             szoveg      = $("#inputMezo").val()
             hanyadiksor = parseInt(hanyadiksor)+1
-            osszessor = hanyadiksor
 
             $("ul").append(` <li   class="sor"  id="sor${hanyadiksor}"   data-szamozo="${hanyadiksor}">
                                 ${szoveg}
@@ -32,9 +26,8 @@ $("#fogomb").click (()  => {
 
 
 /*
-T: minta
-T: Dupla katt: piros-fekete betűszín-váltás.
-Ne töröld ki, mert ez minta.
+T: minta:  Ne töröld ki, mert ez minta.
+T: Dupla kattra piros-fekete betűszín-váltás (dblclick, 2 eset).
 
 let most = null
 $("ul").on("dblclick","li",(event) => {
@@ -52,7 +45,6 @@ else if (`${most}` !== "rgb(0, 0, 0)")   {
 
 
 
-
 // T: Törlés gombra kattintva az adott sort mindenestől kitörli.
 let mittorol = null
 
@@ -64,14 +56,10 @@ $("ul").on("click", ".torl", (event) => {
 
 
 
-
-
-
-
 /*
 //..............................................................................................................
-// T: Törlés gombra kattintva az adott sort mindenestől kitörli.
-
+// T: Törlés gombra kattintva az adott sort mindenestől kitörli. Egy külön megerősító ablak nyílt volna fel.Ez csak próbálkozás.
+// data-aszamozo értékét hogy viszi tovább? Most mindent kitöröl.
 let megerositetttorles = (mittorol) => {
     $("#tenylegtorlom").click(() => {
             $("#sor"+mittorol).remove()
@@ -80,43 +68,28 @@ let megerositetttorles = (mittorol) => {
     )
 }
 
-
-
 let mittorol = null
 
 $("ul").on("click", ".torl", (event) => {
-
     mittorol = $(event.currentTarget).attr("data-szamozo")
                                     console.log(mittorol)   //  pl. a 4-dikre kattintok
     $(".lathato").toggleClass("rejtett")
-
-
                                     megerositetttorles("mittorol")
-
-
 }
 )
-
-
                                     console.log(mittorol)  
-// .................................Kérdés: Hogy lehet itt is megjegyeztetni a fenti data-szamozo értékét?................
-
-
-// ..................................lesz egy függvényem (70-73. sor), aminek a bemenete lesz a mittorol és a 61. sorban fogom meghívni a fg-t.            
 //.......................................................................................................................
 */
 
 
-    
-
-
-
+ 
 // T: 1 katt: kék-sárga... háttérszín váltás. Listasor háttérszíne.
 katt = 0
 
 $("ul").on("click","li",(event) => {
                                     //   console.log(event)
                                     //   console.log(event.target)
+
         // T: törlés gomb háttérszíne akkor sem változik meg, ha a Törlés gombra kattintunk.
         // event.target tartalmazza-e a torl osztályt?
         //Ha a Törlés gombra kattintok, akkor a valt nevű változóra true értéket ad.                                      
@@ -149,8 +122,7 @@ $("ul").on("click","li",(event) => {
 
 
 // T: Színminta az inputmező felett (ikonhoz, betűszínhez kapcsolódik.
-let szinekikon = ["red", "blue", "green", "white", "yellow", "pink", "black"] 
-
+let szinekikon = ["red", "blue", "green", "white", "black", "yellow", "pink"] 
 
 szinekikon.forEach(function (szinikon) {
     $(".kontenerikon").append("<div class='szinboxikon'> </div>")
@@ -160,64 +132,49 @@ szinekikon.forEach(function (szinikon) {
 
 
 
-
-
 // T: Ikonra kattintva a listaelem betűszíne változik meg (input feletti színsáv szerint).
 kattikon = 0
 let mitszinezikon = null
 
 $("ul").on("click",".ikon",(event) => {
-                                        console.log(event)
+                                       // console.log(event)
     kattikon = kattikon + 1
-                                       console.log(kattikon)
-
+                                       // console.log(kattikon)
     mitszinezikon = $(event.currentTarget).attr("data-szamozo")
-                                        console.log(mitszinezikon)
-
+                                       // console.log(mitszinezikon)
             if (kattikon === 1) {
                 $("#sor"+mitszinezikon).css("color", "red")  }       
-//                $(li).css("color", "red")    }
             else if (kattikon === 2) {
                 $("#sor"+mitszinezikon).css("color", "blue")  }       
-//                $(event.target).css("background-color", "rgb(255, 204, 0)")     }
             else if (kattikon === 3) {
                 $("#sor"+mitszinezikon).css("color", "green")  }       
-//                $(event.target).css("background-color", "rgb(255, 119, 0)")     }
             else if (kattikon === 4) {
                 $("#sor"+mitszinezikon).css("color", "white")  }       
-//                $(event.target).css("background-color", "rgb(212, 230, 135)")   }
             else if (kattikon === 5) {
-                $("#sor"+mitszinezikon).css("color", "yellow")  }       
-//               $(event.target).css("background-color", "#c5dae4")               }
+                $("#sor"+mitszinezikon).css("color", "black")  }       
             else if (kattikon === 6) {
-                $("#sor"+mitszinezikon).css("color", "pink")  }       
- //               $(event.target).css("background-color", "#fbebfe")              }
+                $("#sor"+mitszinezikon).css("color", "yellow")  }       
             else if (kattikon === 7) {
-                $("#sor"+mitszinezikon).css("color", "black")         
-//            $(event.target).css("background-color", "rgb(255, 255, 255)")   
+                $("#sor"+mitszinezikon).css("color", "pink")         
     kattikon = 0                                                                    }
 }
 )
 
 
 
-  
-
-
 // T: Színminta az inputmező alatt.
 let szinek = ["rgb(54, 105, 173)", "rgb(255, 204, 0)", "rgb(255, 119, 0)", "rgb(212, 230, 135)", "#c5dae4", "#fbebfe", "rgb(255, 255, 255)"] 
-
 
 szinek.forEach(function (szin) {
     $(".kontener").append("<div class='szinbox'> </div>")
     $(".kontener div:last-of-type").css("background-color", szin)
-//    $(".kontener div:last-of-type").css("display", inline)
+//    $(".kontener div:last-of-type").css("display", inline)    ez a sor a videóban volt, de itt nem működött.
 }
 )
 
 
 
-/* Ez a kettő ugyanaz Függvény definiálás      =>   ill.  function  -nal
+/* Ez a kettő ugyanaz: Függvény definiálás      =>   ill.  function  -nal
 $("#ujelem").click( () => {
 ............................
 }
@@ -245,17 +202,15 @@ function veletlen(number) {
 }
 
 $("#veletlenhatter").click( function() {
-        console.log("háttérgomb")
-    const ujvaltozat = 'rgb(' + veletlen(255) + ',' + veletlen(255) + ',' + veletlen(255) + ')'
-$("body").css("background-color", ujvaltozat)
+                            // console.log("háttérgomb")
+        const ujvaltozat = 'rgb(' + veletlen(255) + ',' + veletlen(255) + ',' + veletlen(255) + ')'
+        $("body").css("background-color", ujvaltozat)
 }
 )
-
-
 /*
 Mit csináltam tartalmilag?
 Először definiálok egy véletlen nevű fg-t, ahol number a bemenet. Ez a fg. kiad egy 0-266 közötti számot
-Math.floor a szám egész része, Math(random) 0=<ilyen számot vletlenszerűen generál=<1, szorozza a numberral (majd 255 lesz a beadott number a fg meghívásakor)
+Math.floor a szám egész része, Math(random) 0=<ilyen számot véletlenszerűen generál<1, szorozza a numberral (majd 255 lesz a beadott number a fg meghívásakor)
 így a végén 0 és 255 közötti vagy azzal egyenlő véletlen szám lesz a return visszaadása a fg-nek.
 #veletlenhatter id-jű gombra kattintva meghívódik az előbb definiált fg, majd a body háttérszín eszerint változik.
 !!!!!!!!!!!!!!!!!!!ujváltozat ne legyen idézőjelben !!!!!!!!!!!!!!!!!!!!!!!//
